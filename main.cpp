e81e20be09b6eb1316bfbe49130a8428ce6bafeb
snuggi <60306809+snuggi@users.noreply.github.com>
2020-07-03 11:49:06 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include "lexical.h"
-
 
 void getFile(std::ifstream &file);
 void scanFile(std::ifstream &file);
 
 int main(int argc, const char** argv) {
+    std::ifstream testFile;
+    getFile(testFile);
+    scanFile(testFile);
     return 0;
 }
 
@@ -18,11 +21,11 @@ void getFile(std::ifstream &file) {
     while(true) {
         std::cin >> filename;
         file.open(filename);
-        if(!file) { std::cout << "File cannot be found."; }
+        if(!file) { std::cout << "File cannot be found. Enter valid filename: "; }
         else { break; }
     }
 }
 
 void scanFile(std::ifstream &file) {
-    
+    std::cout << "TODO";
 }
