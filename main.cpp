f4f09ba6fae690b8ba3397447297f7596262f3d2
snuggi <60306809+snuggi@users.noreply.github.com>
2020-07-03 11:41:52 -0700

    changed. other way too difficult rn lmao

--- main.cpp
+++ main.cpp
@@ -1,48 +1,28 @@
-#ifndef UNICODE
-#define UNICODE
-#define UNICODE_WAS_UNDEFINED
-#endif
-
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <windows.h>
 #include "lexical.h"
 
-#ifdef UNICODE_WAS_UNDEFINED
-#undef UNICODE
-#endif
 
-
-void getFile(std::string filename);
-void scanFile(std::string filename);
+void getFile(std::ifstream &file);
+void scanFile(std::ifstream &file);
 
 int main(int argc, const char** argv) {
-    WIN32_FIND_DATA FindFileData;
-    HANDLE hFindFile;
-    LPCWSTR file = L"C:\\lexicalanalysis\\*.txt";
-    hFindFile = FindFirstFile(file, &FindFileData);
-    if (INVALID_HANDLE_VALUE == hFindFile) { std::cout << GetLastError() << std::endl; }
-    else {
-        std::wstring ws(FindFileData.cFileName);
-        std::string s(ws.begin(), ws.end());
-        // getFile(s);
-        // scanFile(s);
-        std::cout << s << std::endl;
-    }
-    while(FindNextFile(hFindFile, &FindFileData)) {
-        std::wstring ws(FindFileData.cFileName);
-        std::string s(ws.begin(), ws.end());
-        // getFile(s);
-        // scanFile(s);
-        std::cout << s << std::endl;
-    }
-    FindClose(hFindFile);
     return 0;
 }
 
-void getFile(std::string filename) {
-    // if(!filename) {
-    //     std::cout << "Invalid file." << std::endl;
-    // }
+/* Checking whether file exists in local directory */
+void getFile(std::ifstream &file) {
+    std::string filename;
+    std::cout << "Enter filename for lexical analysis: ";
+    while(true) {
+        std::cin >> filename;
+        file.open(filename);
+        if(!file) { std::cout << "File cannot be found."; }
+        else { break; }
+    }
+}
+
+void scanFile(std::ifstream &file) {
+    
 }
