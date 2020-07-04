33c551622415255dd7a33ebea6628c41c0491591
jaredcast <jaredcast97@gmail.com>
2020-07-03 17:05:08 -0700

    Setting up scanFile

--- lexical.cpp
+++ lexical.cpp
@@ -1,1 +1,11 @@
 #include "lexical.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+void printFunction(std::string temp)
+{
+    std::cout << temp;
+}
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include "lexical.h"
+#include "lexical.cpp"
 
 void getFile(std::ifstream &file);
 void scanFile(std::ifstream &file);
@@ -19,7 +20,9 @@ void getFile(std::ifstream &file) {
     std::string filename;
     std::cout << "Enter filename for lexical analysis: ";
     while(true) {
-        std::cin >> filename;
+        //std::cin >> filename;
+        
+        std::string filename = "test1.txt"; //keeping this temporarily here so its easier
         file.open(filename);
         if(!file) { std::cout << "File cannot be found. Enter valid filename: "; }
         else { break; }
@@ -27,5 +30,22 @@ void getFile(std::ifstream &file) {
 }
 
 void scanFile(std::ifstream &file) {
-    std::cout << "TODO";
+    int x = 0;
+    std::string temp;
+    std::string text;
+    std::cout << "\n";
+    while (file >> temp) {
+        //std::cout << temp << std::endl;
+        std::cout << temp << " ";
+        text.append(temp + " ");
+    }
+    std::cout << "\n";
+    std::cout << "Printing with function: ";
+    printFunction(text);
+
+    //Testing the indexes of the string
+    for (int y = 0; y < text.length(); y++)
+    {
+        std::cout << text[y] << " " << y << std::endl;
+    }
 }
